e0c2249d6b6e93192af460603663271f8aac9009
SungmanYou <sungman.you@gmail.com>
2017-08-13 20:08:40 +0900

    include comments

--- static/socket.js
+++ static/socket.js
@@ -5,8 +5,9 @@ var init = new Promise((resolve, reject) => {
     socket.emit("init", (data) => {
 	if (data != null) {
 	    console.log(data);
-	    // If the returned "data" is not null,
-	    // the "resolve" function will be called. 
+	    // If the returned "data" is not null, the "resolve" function will be called.
+	    // At this point, the "resolve" function is NOT defined.
+	    // It will be defined when this Promise function is called.
 	    resolve(data);
 	}
 	else {
